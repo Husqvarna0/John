337617cbceb55379805643e328d700e9b6fe5237
Husqvarna_0 <dancewithdead21@gmail.com>
2023-05-10 19:01:17 +0300

    add blog page

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,20 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+
+function moveMenuElementToFirstPlace() {
+
+	if (document.location.pathname === '/blog.html') {
+		let menu = document.getElementById('menu__list');
+		let firstElement = menu.firstElementChild;
+		let secondElement = menu.children[1];
+
+		menu.insertBefore(secondElement, firstElement);
+		secondElement.classList.add('open');
+	}
+}
+
+window.onload = moveMenuElementToFirstPlace;
+
